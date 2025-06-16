7639e38f7f0bc14ab8c469bef40e6892e01205b3
bilalsxadad1231231 <112323303+bilalsxadad1231231@users.noreply.github.com>
2025-06-16 18:37:35 +0500

    Prepare for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
+        manualChunks: undefined,
         assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
