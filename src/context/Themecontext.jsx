eaf21b58b1ad78beb7c81e0a5335508d9e72c951
bilalsxadad1231231 <112323303+bilalsxadad1231231@users.noreply.github.com>
2025-06-17 12:19:31 +0500

    feat: Set default theme to blue

--- src/context/Themecontext.jsx
+++ src/context/Themecontext.jsx
@@ -5,7 +5,7 @@ const ThemeContext = createContext();
 
 export const ThemeProvider = ( {children} ) =>{
 
-    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "blue");
 
 
     useEffect(() => {
